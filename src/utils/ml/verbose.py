2f38c37168b4c5d2bb6b4de14e1d3e9aa9ccb007
GSimonX37 <GSimonX37@gmail.com>
2024-05-09 15:30:22 +0700

    Add information and core number

--- src/utils/ml/verbose.py
+++ src/utils/ml/verbose.py
@@ -3,11 +3,30 @@ from optuna.trial import FrozenTrial
 
 
 class Verbose(object):
-    def __init__(self, trials: int, name: str):
+    """
+    Класс для отображения прогресса подбора гиперпараметров с помощью Optuna.
+    """
+
+    def __init__(self, trials: int, name: str, n_job: int):
+        """
+        :param trials: общее количество испытаний;
+        :param name: название модели;
+        :param n_job: номер исследования;
+        """
+
         self.trials = trials
         self.name = name
+        self.n_job = n_job
 
-    def __call__(self, study: Study, trial: FrozenTrial):
+    def __call__(self, study: Study, trial: FrozenTrial) -> None:
+        """
+        Вызывается после каждого испытания;
+
+        :param study: задача оптимизации - совокупность испытаний;
+        :param trial: крайнее испытание;
+        :return: None.
+        """
+
         index = trial.number + 1
         state = trial.state.name
         complete = (trial
@@ -19,5 +38,6 @@ class Verbose(object):
         value = round(trial.values[0], 4)
         best = round(study.best_value, 4)
 
-        print(f'{self.name}: [{complete}] - [{minutes:02}:{seconds:02}] - '
+        print(f'{self.name} [{self.n_job}]: '
+              f'[{complete}] - [{minutes:02}:{seconds:02}] - '
               f'{state}: {index}/{self.trials} - {value:.4f} ({best:.4f}).')
